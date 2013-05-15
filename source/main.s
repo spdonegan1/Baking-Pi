d73cba465a065bf8895255842497598b6da3024d
Sean Donegan <spdonegan1@gmail.com>
2013-05-15 01:14:38 -0400

    Complete lesson 2. Turn on/off OK LED.

--- source/main.s
+++ source/main.s
@@ -1,13 +1,39 @@
 .section .init
 .globl _start
-_start: 
-ldr r0, =0x20200000
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
-mov r1,#1
-lsl r1,#16
-str r1,[r0,#40]
-loop$: 
-b loop$
+_start:
 
+/* load the GPIO region address into r0 */
+ldr r0, =0x20200000
+
+mov r1, #1
+lsl r1, #18
+
+str r1, [r0,#4]
+
+lsr r1, #2
+
+loop$: 
+
+    /* turn on led */
+    str r1,[r0,#40]
+
+    /* set delay for timer */
+    mov r2,#0x3F0000
+
+    wait1$:
+        sub r2,#1
+        cmp r2,#0
+        bne wait1$
+
+    /* turn off led */
+    str r1,[r0,#28]
+
+    /* reset timer delay */
+    mov r2,#0x3F0000
+
+    wait2$:
+        sub r2,#1
+        cmp r2,#0
+        bne wait2$
+
+b loop$
